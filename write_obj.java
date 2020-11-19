d05a8692e8e76c25f98adb8e6622ffc13f7555c7
leftside97 <l3f7s1d3@protonmail.com>
2020-11-19 16:53:10 +0100

    Create write_obj.java

---
+++ write_obj.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class ObjectSave {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DataObject dataobj = new DataObject("lolig");
+		try {
+			File datei = new File("/home/user/obj.dat");
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(datei));
+			oos.writeObject(dataobj);
+			oos.flush();
+			oos.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
