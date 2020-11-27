0d15f202e8d7ddc123676eb162b81237bf1987aa
leftside97 <l3f7s1d3@protonmail.com>
2020-11-27 22:26:27 +0100

    Create FileCipher.java

---
+++ FileCipher.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RUN {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
+		
+		ArrayHandler bah = new ArrayHandler();
+
+		byte[] data;
+		
+    System.out.println("key:");
+    	
+		aesbytecipher abc = new aesbytecipher(reader.readLine());
+
+		switch(args[0]){
+        case "-enc":
+    		  data = abc.enc(bah.read(args[1]));
+    		  bah.write(data, args[1]+".enc");
+        	break;
+        case "-dec":
+        	data = abc.dec(bah.read(args[1]));
+        	bah.write(data, args[1]+".dec");
+        	break;
+        default:
+        	System.out.println("!");
+        	break;
+		}		
+	}
+}
