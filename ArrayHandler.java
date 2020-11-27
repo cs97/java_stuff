6a88ea0676960cfcad49b6932e9f3a2e72f72841
leftside97 <l3f7s1d3@protonmail.com>
2020-11-27 21:50:22 +0100

    Create ArrayHandler.java

---
+++ ArrayHandler.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ArrayHandler {
+	public byte[] read(String s) {
+		try {
+			byte[] bytes = Files.readAllBytes(Paths.get(s));
+			return bytes;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void write(byte[] b, String s) {
+		try {
+			Files.write(Paths.get(s), b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
