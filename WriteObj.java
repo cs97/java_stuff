16984974a6fb9c1e17a0b7604496d57aaf7d53df
leftside97 <l3f7s1d3@protonmail.com>
2020-11-27 23:12:20 +0100

    Update WriteObj.java

--- WriteObj.java
+++ WriteObj.java
@@ -6,7 +6,7 @@ public class WriteObj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DataObject dataobj = new DataObject("lolig");
+		DataObject dataobj = new SimpleDataObject("weed");
 		try {
 			File datei = new File("/home/user/obj.dat");
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(datei));
