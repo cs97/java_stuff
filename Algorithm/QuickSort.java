c784c4ccb64bab7b855b2f49649e097ac168c1f3
leftside97 <l3f7s1d3@protonmail.com>
2021-02-25 12:20:39 +0100

    Update QuickSort.java

--- Algorithm/QuickSort.java
+++ Algorithm/QuickSort.java
@@ -24,11 +24,7 @@ public class QuickSort {
 		while(l < r) {
 			while (S[l] < x) l++;
 			while (!(S[r] < x)) r--;
-			if (l < r) {
-				int tmp = S[l];
-				S[l] = S[r];
-				S[r] = tmp;
-			}
+			if (l < r) swap(S, l, r);
 		}
 		return l;
 	}
@@ -40,14 +36,10 @@ public class QuickSort {
 		else if (S[k-1] < S[k]) return k; else return k-1;		
 	}
 	
-	public static void main(String[] args) {
-		
-		int data[] = {1,4,1,4,2,1,3,5,6,2};
-		System.out.println("input");
-		System.out.println(Arrays.toString(data));
-
-		quickSort(data);
-		System.out.println("output");
-		System.out.println(Arrays.toString(data));
+	
+	private static void swap(int[] S, int one, int two) {
+		int tmp=S[one];
+		S[one]=S[two];
+		S[two]=tmp;
 	}
 }
